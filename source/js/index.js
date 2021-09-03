e5bfade0c2f753d30e13a49a9b62da7909ff23ef
Eduard <eduard66_06@list.ru>
2021-09-03 20:05:15 +1200

    fix: footer

--- source/js/index.js
+++ source/js/index.js
@@ -3,6 +3,9 @@ const nav = document.querySelector('.nav-menu');
 const navToggle = document.querySelector('.page-header__burger-btn');
 
 
+const ppBtn = document.querySelector('.page-footer__pp-btn');
+const ppText = document.querySelector('.page-footer__pp-text');
+
 if (nav) {
   nav.classList.remove('nav-menu--no-js');
 }
@@ -13,5 +16,16 @@ if (navToggle) {
     nav.classList.toggle('nav-menu--open');
     header.classList.toggle('page-header--close');
     navToggle.classList.toggle('page-header__burger-btn--active');
+  })
+}
+
+if (ppText) {
+  ppText.classList.add('page-footer__pp-text--close');
+}
+
+if (ppBtn) {
+  ppBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    ppText.classList.toggle('page-footer__pp-text--close');
   })
 }
